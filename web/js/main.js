869b335dbc66d10f2d7687b90ac4c5cf2c6b65af
s_murakami <s.murakami@conol.jp>
2014-06-09 18:39:10 +0900

    navを上にはりつける実装完了

--- web/js/main.js
+++ web/js/main.js
@@ -1,12 +1,30 @@
 var main = {
   init: function() {
-    console.log("hello");
     this.initProps();
+    this.scroll.init();
     this.top.init();
+    this.nav.init();
     this.gallery.init();
   },
   initProps: function() {
     // デフォルトの要素サイズを取得しておく
+  },
+};
+
+main.scroll = {
+  init: function() {
+    // var WIDTH = $(window).width();
+    // var HEIGHT = $(window).height();
+    $(window).scroll(function(e){
+      var top = $(document).scrollTop();
+      if (top > $('#top').height()) {
+        $('#nav').addClass('stick-to-top');
+        $('#nav-alt').removeClass('hidden');
+      } else {
+        $('#nav').removeClass('stick-to-top');
+        $('#nav-alt').addClass('hidden');
+      }
+    });
   },
 };
 
@@ -16,10 +34,16 @@ main.top = {
   },
 };
 
+main.nav = {
+  init: function () {
+
+  },
+};
+
 main.gallery = {
   init: function () {
   },
-}
+};
 
 $(function(){
   main.init();
